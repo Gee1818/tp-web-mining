0d581bf036329b92d3bcddc33e0373eb34aee39c
Victorio Costa <162653514+vcosta10@users.noreply.github.com>
2025-03-14 20:40:14 -0300

    Completado scrape_cinemark
    Agregué ahora sí para guardar las pelis de Cinemark, escribí el csv correspondiente y acomodé el archivo limpieza.R.

--- limpieza.R
+++ limpieza.R
@@ -1,16 +1,47 @@
+# Mergeo de los datasets obtenidos de cada web
+# ------------------------------------------
 library(tidyverse)
 
-pelis_showcase  <- read.csv('pelis_showcase.csv')
-pelis_cinepolis <- read.csv('pelis_cinepolis.csv')
-pelis_centro    <- read.csv('pelis_centro.csv')
-pelis_tipas     <- read.csv('pelis_tipas.csv')
+# Carga de los archivos csv
+pelis_showcase   <- read.csv('pelis_showcase.csv')
+pelis_cinepolis  <- read.csv('pelis_cinepolis.csv')
+pelis_centro     <- read.csv('pelis_centro.csv')
+pelis_tipas      <- read.csv('pelis_tipas.csv')
+pelis_monumental <- read.csv('pelis_monumental.csv')[, -1]
+pelis_cinemark   <- read.csv('pelis_cinemark.csv')
 
-pelis_showcase$Cine  <- "Showcase"
-pelis_cinepolis$Cine <- "Cinépolis"
-pelis_centro$Cine    <- "Cines del Centro"
-pelis_tipas$Cine     <- "Las Tipas"
+# Corrijo los formatos de cada dataset segun sea necesario
+pelis_centro <- pelis_centro %>% 
+  mutate(Fecha = ymd(Fecha))
 
-pelis <- rbind(pelis_showcase, pelis_cinepolis, pelis_centro, pelis_tipas)
+pelis_cinemark <- pelis_cinemark %>% 
+  mutate(
+    Fecha = dmy(paste0(data$Fecha, "/", year(Sys.Date()))),
+    Horario = gsub(" ", "", Horario)
+    )
+
+pelis_cinepolis <- pelis_cinepolis %>% 
+  mutate(Fecha = ymd(Fecha))
+
+pelis_monumental <- pelis_monumental %>% 
+  mutate(Fecha = ymd(Fecha))
+
+pelis_showcase <- pelis_showcase %>% 
+  mutate(Fecha = ymd(Fecha))
+
+pelis_tipas <- pelis_tipas %>% 
+  mutate(Fecha = ymd(Fecha))
+
+
+# Agrego una columna con la indicadora 
+pelis_showcase$Cine   <- "Showcase"
+pelis_cinepolis$Cine  <- "Cinépolis"
+pelis_centro$Cine     <- "Cines del Centro"
+pelis_tipas$Cine      <- "Las Tipas"
+pelis_monumental$Cine <- "Monumental"
+pelis_cinemark$Cine   <- "Cinemark Hoyts"
+
+pelis <- rbind(pelis_showcase, pelis_cinepolis, pelis_centro, pelis_tipas, pelis_monumental, pelis_cinemark)
 
 pelis %>% pull(Tipo) %>% unique() #%>% length()
 pelis %>% filter(Cine == "Showcase") %>% pull(Tipo) %>% unique()
--- scrape_cinemark.R
+++ scrape_cinemark.R
@@ -15,70 +15,100 @@
 #          III. Para cada tipo de funcion, extraer opciones de horarios
 
 library(RSelenium)
-# library(rvest)
 library(netstat)
 library(tidyverse)
 library(wdman)
 library(rlist)
 
-# Cerrar puerto en uso
+# 0. Cerrar puerto en uso (si lo hubiera)
 remDr$close()
 remote_driver$server$stop()
 
-# Iniciar el servidor de Selenium con GeckoDriver (Firefox)
+## 1. Iniciar el servidor de Selenium con GeckoDriver (Firefox)
 remote_driver <- rsDriver(browser = "firefox", chromever = NULL, verbose = F)
 remDr <- remote_driver$client
 
-# 1. Acceder a la pagina
+## 2. Acceder a la pagina
 remDr$navigate("https://www.cinemarkhoyts.com.ar/")
 Sys.sleep(5)
 
-#  2. Cerrar el anuncio emergente
+## 3. Cerrar el anuncio emergente
 btn_cookies <- remDr$findElement(using = "css selector", ".ch-popup-section-content > button")
 remDr$executeScript("arguments[0].click();", list(btn_cookies))
 
-#  3. Obtener el catalogo para el cine de Rosario
+## 4. Obtener el catalogo para el cine de Rosario
 selector_cine <- remDr$findElements(using = "css selector", "select.ch-select")[[1]]
 selector_cine$sendKeysToElement(list(key="enter"))
 cine_ros <- remDr$findElements(using = "css selector", "li.opt")[[16]]
 remDr$executeScript("arguments[0].click();", list(cine_ros))
 
+
+## 5. Se obtienen las peliculas del desplegable para iterar
 selector_peli <- remDr$findElements(using = "css selector", "select.ch-select")[[2]]
 selector_peli$sendKeysToElement(list(key="enter"))
 pelis <- remDr$findElements(using = "css selector", "li.opt")
+
+# Funcion para obtener los indices de las peliculas en el desplegable
 indice_peli1 <- function(y) {
   nombres_pelis <- unlist(lapply(y, function(x) x$getElementText()[[1]]))
   which(nombres_pelis != "")[1]
 }
 indice <- indice_peli1(pelis)
+
+# Creacion del dataset para almacenar la informacion
+data <- data.frame(Nombre = NULL, Fecha = NULL, Tipo = NULL, Horario = NULL)
+
+## 6. Iteracion en la pagina para obtener la informacion deseada 
+# Para cada pelicula
 for (i in 26:length(pelis)) {
+  # Obtengo el nombre de la pelicula
   nombre_peli <- pelis[[indice+i-26]]$getElementText()[[1]]
-  print(nombre_peli)
   remDr$executeScript("arguments[0].click();", list(pelis[[indice+i-26]]))
   Sys.sleep(1)
+  # Cierro (si existe) el cartel de advertencia de que no hay pelicula en la fecha seleccionada
   popup_advertencia <- remDr$findElement(using = "css selector", "div#ch-popup-confirm > button")
   popup_advertencia$clickElement()
   Sys.sleep(1)
   
+  # Obtengo las fechas disponibles
   fechas <- remDr$findElements(using = "css selector", "div#ch-timeline > div.ch-radio")
   
+  # Para cada fecha
   for (fecha in fechas) {
+    # Obtengo la fecha de la pelicula
     fecha$clickElement()
     Sys.sleep(1)
     fecha_text <- strsplit(fecha$getElementText()[[1]], "\n")[[1]][2]
-    print(fecha_text)
-    formatos <- remDr$findElements(using = "css selector", "ul.list-group.p-0 > li")
-    for (formato in formatos) {
-      formato_text <- formato$findChildElement(using = "css selector", "h2.ch-showtime-title")$getElementText()[[1]]
-      print(formato_text)
-      horarios <- formato$findChildElements(using = "css selector", "ul .ch-radio.ch-radio_outline")
-      #print(unlist(lapply(horarios, function(x) x$getElementText()[[1]])))
-      print(unlist(lapply(horarios, function(x) x$getElementAttribute("textContent")[[1]])))
+    
+    # Obtengo los tipos (2D/3D, subtitulada/doblada)
+    tipos <- remDr$findElements(using = "css selector", "ul.list-group.p-0 > li")
+    
+    # Para cada tipo
+    for (tipo in tipos) {
+      # Obtengo el tipo de la pelicula
+      tipo_text <- tipo$findChildElement(using = "css selector", "h2.ch-showtime-title")$getElementText()[[1]]
+      
+      # Obtengo los diferentes horarios
+      horarios <- tipo$findChildElements(using = "css selector", "ul .ch-radio.ch-radio_outline")
+      
+      # Para cada horario
+      for (horario in horarios) {
+        # Obtengo el horario de la pelicula
+        horario_text <- horario$getElementAttribute("textContent")[[1]]
+        
+        # Printeo el resultado en la consola y lo agrego al dataset  
+        print(c(nombre_peli, fecha_text, tipo_text, horario_text))
+        data <- rbind(data, data.frame(Nombre = nombre_peli, Fecha = fecha_text, Tipo = tipo_text, Horario = horario_text))
+      }
     }
   }
   
+  # "Reseteo" los valores iniciales para recuperar el orden inicial y seguir con la iteracion
   selector_peli <- remDr$findElements(using = "css selector", "select.ch-select")[[2]]
   selector_peli$sendKeysToElement(list(key="enter"))
   pelis <- remDr$findElements(using = "css selector", "li.opt")
   indice <- indice_peli1(pelis)
 }
+
+# Se exporta el dataset en formato csv
+write.csv(data, "./pelis_cinemark.csv", row.names = F, fileEncoding = "UTF-8")
