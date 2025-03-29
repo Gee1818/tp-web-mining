83ff96da1d7175d5461fac674f07ea7e47b304ea
bjoaquin <joaquin.bmjo@gmail.com>
2025-03-29 12:37:13 -0300

    limpieza de formatos

--- limpieza.R
+++ limpieza.R
@@ -1,36 +1,71 @@
 # Mergeo de los datasets obtenidos de cada web
 # ------------------------------------------
 library(tidyverse)
+library(stringr)
+
+# Set the working directory to the folder in which the script is saved
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 # Carga de los archivos csv
 pelis_showcase   <- read.csv('pelis_showcase.csv')
-pelis_cinepolis  <- read.csv('pelis_cinepolis.csv')
 pelis_centro     <- read.csv('pelis_centro.csv')
+pelis_cinemark   <- read.csv('pelis_cinemark.csv')
+pelis_cinepolis  <- read.csv('pelis_cinepolis.csv')
 pelis_tipas      <- read.csv('pelis_tipas.csv')
 pelis_monumental <- read.csv('pelis_monumental.csv')[, -1]
-pelis_cinemark   <- read.csv('pelis_cinemark.csv')
 
 # Corrijo los formatos de cada dataset segun sea necesario
 pelis_centro <- pelis_centro %>% 
-  mutate(Fecha = ymd(Fecha))
+  mutate(
+    Fecha = ymd(Fecha),
+    FormatoImagen = "2D",
+    FormatoIdioma = case_when(
+      Tipo == "SUB" ~ "Subtitulado", 
+      .default = "Español"
+    )
+  )
 
 pelis_cinemark <- pelis_cinemark %>% 
   mutate(
-    Fecha = dmy(paste0(data$Fecha, "/", year(Sys.Date()))),
-    Horario = gsub(" ", "", Horario)
+    Fecha = dmy(paste0(Fecha, "/", year(Sys.Date()))),
+    Horario = gsub(" ", "", Horario),
+    Tipo = str_remove(Tipo, fixed("DBOX + ")),
+    FormatoImagen = str_extract(Tipo, "^[^\\ ]+"),
+    FormatoIdioma = str_extract(Tipo, "(?<=\\ ).*"),
+    FormatoIdioma = if_else(FormatoIdioma == "CASTELLANO", "Español", "Subtitulado")
     )
 
 pelis_cinepolis <- pelis_cinepolis %>% 
-  mutate(Fecha = ymd(Fecha))
+  mutate(
+    Fecha = ymd(Fecha),
+    FormatoIdioma = str_extract(Tipo, "(?<= • ).*"),
+    FormatoImagen = str_extract(FormatoIdioma, "^[^ • ]+"),
+    FormatoIdioma = str_extract(FormatoIdioma, "(?<= • ).*")
+  )
 
 pelis_monumental <- pelis_monumental %>% 
-  mutate(Fecha = ymd(Fecha))
+  mutate(
+    Fecha = ymd(Fecha),
+    Horario = str_remove(Horario, "\\*"),
+    FormatoImagen = "2D",
+    FormatoIdioma = if_else(Tipo == "doblada", "Español", "Subtitulado"),
+  )
 
 pelis_showcase <- pelis_showcase %>% 
-  mutate(Fecha = ymd(Fecha))
+  mutate(
+    Fecha = ymd(Fecha),
+    FormatoImagen = str_extract(Tipo, "^[^-]+"),
+    FormatoIdioma = str_extract(Tipo, "(?<=-).*"),
+    FormatoIdioma = if_else(is.na(FormatoIdioma), "Español", FormatoIdioma)
+  )
 
 pelis_tipas <- pelis_tipas %>% 
-  mutate(Fecha = ymd(Fecha))
+  mutate(
+    Fecha = ymd(Fecha),
+    FormatoImagen = str_extract(Tipo, "^[^\\ ]+"),
+    FormatoIdioma = str_extract(Tipo, "(?<=\\ ).*"),
+    FormatoIdioma = if_else(is.na(FormatoIdioma), "Español", "Subtitulado")
+  )
 
 
 # Agrego una columna con la indicadora 
@@ -41,7 +76,14 @@ pelis_tipas$Cine      <- "Las Tipas"
 pelis_monumental$Cine <- "Monumental"
 pelis_cinemark$Cine   <- "Cinemark Hoyts"
 
-pelis <- rbind(pelis_showcase, pelis_cinepolis, pelis_centro, pelis_tipas, pelis_monumental, pelis_cinemark)
+pelis <- rbind(
+  pelis_centro, 
+  pelis_cinemark,
+  pelis_cinepolis, 
+  pelis_monumental, 
+  pelis_showcase, 
+  pelis_tipas, 
+)
 
 pelis %>% pull(Tipo) %>% unique() #%>% length()
 pelis %>% filter(Cine == "Showcase") %>% pull(Tipo) %>% unique()
